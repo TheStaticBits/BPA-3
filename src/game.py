43b6668691685a66e10f6926a1cc28c4d47e4b06
TheStaticBits <puppy2655@gmail.com>
2023-09-10 17:00:42 -0400

    Creates a window with FPS outputting

--- src/game.py
+++ src/game.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, logging
 
 import src.utility.utility as util
 from src.window import Window
@@ -11,8 +11,18 @@ class Game:
         self.constants = util.loadJSON(CONSTANTS_FILE)
         util.setupLogger(self.constants)
 
+        self.log = logging.getLogger(__name__)
+
+        self.log.info("Initializing game")
+
         # Window
-        self.window = Window()
+        self.window = Window(self.constants)
+
 
     def mainLoop(self):
-        pass
+        while not self.window.isClosed():
+            self.window.handleInputs()
+
+            # Game logic here
+
+            self.window.update()
--- src/window.py
+++ src/window.py
@@ -1,10 +1,73 @@
-import pygame
-import logging
+import pygame, logging, time
 
 class Window:
-    def __init__(self):
+    def __init__(self, constants):
         self.log = logging.getLogger(__name__)
 
-        self.log.info("e")
-        self.log.warning("Initializing window...")
-        self.log.error("Failed window...")
+        self.log.info("Initializing window")
+        
+        self.window = pygame.display.set_mode(constants["window"]["size"])
+        pygame.display.set_caption(constants["window"]["title"])
+
+        self.clock = pygame.time.Clock()
+
+        self.FPS = constants["window"]["FPS"]
+        self.LOG_FPS = constants["window"]["logFPS"] # True/False
+
+        self.deltaTime = 0
+        self.previousTime = time.time()
+
+        # For finding the average FPS over each second to output to the console
+        self.lastSecondFPS = [] 
+        self.frameTimer = 0 # Used to keep track of when a second has passed
+
+        self.quit = False
+    
+
+    def update(self):
+        """ Updates the window with what was rendered over the previous frame """
+
+        pygame.display.flip() # Update the window
+        self.window.fill((0, 0, 0)) # Clear the window
+
+        # Cap FPS
+        if self.FPS > 0:
+            self.clock.tick(self.FPS)
+
+        # Deltatime is the time that has elapsed since the previous function
+        # Any movement is multiplied by it to make it framerate independent
+        currentTime = time.time()
+        self.deltaTime = currentTime - self.previousTime
+        self.previousTime = currentTime
+
+        self.frameTimer += self.deltaTime
+
+        if self.LOG_FPS:
+            if self.deltaTime != 0:
+                # Changes deltatime into FPS
+                self.lastSecondFPS.append(1 / self.deltaTime)
+
+            if self.frameTimer >= 1:
+                self.frameTimer -= 1
+
+                # Finds the average FPS over the last second
+                avgDeltaTimes = sum(self.lastSecondFPS) / len(self.lastSecondFPS)
+
+                self.log.info(f"{round(avgDeltaTimes, 2)} FPS")
+                self.lastSecondFPS.clear()
+        
+    
+    def handleInputs(self):
+        """ Handle any window inputs """
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.log.info("Exiting game")
+                self.quit = True
+    
+
+    # Getters
+    def getDeltaTime(self):
+        """ Returns the time that has elapsed since the previous function """
+        return self.deltaTime
+
+    def isClosed(self): return self.quit
